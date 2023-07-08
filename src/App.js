445a6dd0a147cd1ca146d68d024fb432f51c3177
Zak-2005 <zak.mob2005@gmail.com>
2023-07-07 21:50:42 -0500

    Add base components and onChange

--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import Field from "./components/Field";
+import Title from "./components/Title";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      phoneNum: "",
+      schoolName: "",
+      titleStudy: "",
+      dateStudy: "",
+      companyName: "",
+      position: "",
+      mainTasks: "",
+      periodWorked: "",
+    };
+
+    this.title = "";
+    this.fields = [];
+
+    this.setPersonalInfo = this.setPersonalInfo.bind(this);
+    this.setEducationInfo = this.setEducationInfo.bind(this);
+    this.setExperienceInfo = this.setExperienceInfo.bind(this);
+  }
+
+  setPersonalInfo(value, subSec) {
+    if (subSec === "Name:") {
+      this.setState((previousState) => ({
+        name: value,
+      }));
+    } else if (subSec === "Email:") {
+      this.setState((previousState) => ({
+        email: value,
+      }));
+    } else {
+      this.setState((previousState) => ({
+        phoneNum: value,
+      }));
+    }
+  }
+  setEducationInfo(value, subSec) {
+    if (subSec === "School Name:") {
+      this.setState((previousState) => ({
+        schoolName: value,
+      }));
+    } else if (subSec === "Title of Study:") {
+      this.setState((previousState) => ({
+        titleStudy: value,
+      }));
+    } else {
+      this.setState((previousState) => ({
+        dateStudy: value,
+      }));
+    }
+  }
+  setExperienceInfo(value, subSec) {
+    if (subSec === "Company Name:") {
+      this.setState((previousState) => ({
+        companyName: value,
+      }));
+    } else if (subSec === "Position:") {
+      this.setState((previousState) => ({
+        position: value,
+      }));
+    } else if (subSec === "Main Tasks at Job:") {
+      this.setState((previousState) => ({
+        mainTasks: value,
+      }));
+    } else {
+      this.setState((previousState) => ({
+        periodWorked: value,
+      }));
+    }
+  }
+  render() {
+    return (
+      <div>
+        <Title title="General Info" />
+        <Field
+          section={[this.state.name, this.state.email, this.state.phoneNum]}
+          onChange={this.setPersonalInfo}
+          fields={[
+            { field: "Name:" },
+            { field: "Email:" },
+            { field: "Phone Number:" },
+          ]}
+        />
+        <Title title="Education" />
+        <Field
+          section={[
+            this.state.schoolName,
+            this.state.titleStudy,
+            this.state.dateStudy,
+          ]}
+          onChange={this.setEducationInfo}
+          fields={[
+            { field: "School Name:" },
+            { field: "Title of Study:" },
+            { field: "Date of Study:" },
+          ]}
+        />
+        <Title title="Experience" />
+        <Field
+          section={[
+            this.state.companyName,
+            this.state.position,
+            this.state.mainTasks,
+            this.state.periodWorked,
+          ]}
+          onChange={this.setExperienceInfo}
+          fields={[
+            { field: "Company Name:" },
+            { field: "Position:" },
+            { field: "Main Tasks at Job:" },
+            { field: "Period Worked" },
+          ]}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Field.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default class Field extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div>
+                {this.props.fields.map((object,index)=>{
+                    return <input key={index} type="text" placeholder={object.field} value={this.props.section[index]} onChange={(e)=>this.props.onChange(e.target.value,object.field)}/>  
+                })}
+                <button></button>
+            </div>
+        )
+    }
+}
