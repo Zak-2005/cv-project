971c2cb91eb0cf25360727e317a97ecab45f9dc3
Zak-2005 <zak.mob2005@gmail.com>
2023-07-09 17:08:09 -0500

    Convert MainOutput to functional component

--- src/MainOutput.js
+++ src/MainOutput.js
@@ -2,75 +2,73 @@ import React from "react";
 import App from "./MainInput";
 import Title from "./components/Title";
 import OutputField from "./components/OutputField";
-export default class MainOutput extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className="output">
-        <div className="mainOutputHeader">
-          <OutputField field={this.props.field.name} class={false} />
-          <div className="contactInfo">
-            <OutputField
-              fieldName="Email:"
-              field={this.props.field.email}
-              class={false}
-            />
-            <OutputField
-              fieldName="Phone Number:"
-              field={this.props.field.phoneNum}
-              class={false}
-            />
-          </div>
-        </div>
-        <div className="mainOutputContent">
-          <Title title="Education" />
-          <div className="section">
-            <OutputField
-              fieldName="School Name:"
-              field={this.props.field.schoolName}
-              class={true}
-            />
-            <OutputField
-              fieldName="Title of Study:"
-              field={this.props.field.titleStudy}
-              class={true}
-            />
 
-            <OutputField
-              fieldName="Date of Study:"
-              field={this.props.field.dateStudy}
-              class={true}
-            />
-          </div>
-        </div>
-        <div className="mainOutputContent">
-          <Title title="Experience" />
-          <div className="section">
-            <OutputField
-              fieldName="Company Name:"
-              field={this.props.field.companyName}
-              class={true}
-            />
-            <OutputField
-              fieldName="Position:"
-              field={this.props.field.position}
-              class={true}
-            />
-            <OutputField
-              fieldName="Main Tasks:"
-              field={this.props.field.mainTasks}
-              class={true}
-            />
-            <OutputField
-              fieldName="Period Worked:"
-              field={this.props.field.periodWorked}
-              class={true}
-            />
-          </div>
+
+ const MainOutput = (props) =>{
+  return (
+    <div className="output">
+      <div className="mainOutputHeader">
+        <OutputField field={props.field.name} class={false} />
+        <div className="contactInfo">
+          <OutputField
+            fieldName="Email:"
+            field={props.field.email}
+            class={false}
+          />
+          <OutputField
+            fieldName="Phone Number:"
+            field={props.field.phoneNum}
+            class={false}
+          />
         </div>
       </div>
-    );
-  }
+      <div className="mainOutputContent">
+        <Title title="Education" />
+        <div className="section">
+          <OutputField
+            fieldName="School Name:"
+            field={props.field.schoolName}
+            class={true}
+          />
+          <OutputField
+            fieldName="Title of Study:"
+            field={props.field.titleStudy}
+            class={true}
+          />
+
+          <OutputField
+            fieldName="Date of Study:"
+            field={props.field.dateStudy}
+            class={true}
+          />
+        </div>
+      </div>
+      <div className="mainOutputContent">
+        <Title title="Experience" />
+        <div className="section">
+          <OutputField
+            fieldName="Company Name:"
+            field={props.field.companyName}
+            class={true}
+          />
+          <OutputField
+            fieldName="Position:"
+            field={props.field.position}
+            class={true}
+          />
+          <OutputField
+            fieldName="Main Tasks:"
+            field={props.field.mainTasks}
+            class={true}
+          />
+          <OutputField
+            fieldName="Period Worked:"
+            field={props.field.periodWorked}
+            class={true}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
+ export default MainOutput;
