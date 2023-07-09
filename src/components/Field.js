5a223b35c8c265a200ecc58fc4083f40e5c62c32
Zak-2005 <zak.mob2005@gmail.com>
2023-07-09 17:46:33 -0500

    Convert Field to function component

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,28 +1,24 @@
 import React from "react";
 
-export default class Field extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div className="fieldSection">
-        {this.props.fields.map((object, index) => {
-          return (
-            <div key={index} className="field">
-              <label>{object.field}</label>
-              <input
-                type="text"
-                placeholder={object.field}
-                value={this.props.section[index]}
-                onChange={(e) =>
-                  this.props.onChange(e.target.value, object.field)
-                }
-              />
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
+const Field = (props) =>{
+  return (
+    <div className="fieldSection">
+      {props.fields.map((object, index) => {
+        return (
+          <div key={index} className="field">
+            <label>{object.field}</label>
+            <input
+              type="text"
+              placeholder={object.field}
+              value={props.section[index]}
+              onChange={(e) =>
+                props.onChange(e.target.value, object.field)
+              }
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
+export default Field;
